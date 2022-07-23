b9cf7691416100bae3485a26f4968e9fc513e46a
gnoul_ <ttluong4869@gmail.com>
2022-07-23 20:04:54 +0700

    Create Hand and Chip classes

--- main.py
+++ main.py
@@ -39,7 +39,47 @@ class Deck:
         return self.cards.pop()
 
 
+class Hand:
+    def __init__(self) -> None:
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def __str__(self) -> str:
+        cards_str = ''
+        for card in self.cards:
+            cards_str += f'\n{card}'
+
+        hand_str = f'Hand has {cards_str}\n with a total value of {self.value}'
+
+        return hand_str
+
+    def add_card(self, card) -> None:
+        self.cards.append(card)
+        self.value += VALUES[card.rank]
+
+        if card.rank == 'Ace':
+            self.aces += 1
+
+        if self.value > 21 and self.aces > 0:
+            self.value -= 10
+            self.aces -= 1
+
+
+class Chip:
+    def __init__(self) -> None:
+        self.total = 100
+        self.bet = 0
+
+    def add_total(self, value) -> None:
+        self.total += value
+
+
 deck = Deck()
 deck.shuffle()
+player_hand = Hand()
+player_hand.add_card(deck.deal())
+player_hand.add_card(deck.deal())
+player_hand.add_card(deck.deal())
 
-print(deck)
+print(player_hand)
